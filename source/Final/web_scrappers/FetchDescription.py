4813c6bcc539caa53c15a687c1b31673cfbf38b3
Anshul Patel <36321866+anshulp2912@users.noreply.github.com>
2021-11-04 10:38:11 -0400

    added final

---
+++ source/Final/web_scrappers/FetchDescription.py
@@ -0,0 +1,125 @@
+"""
+Copyright (c) 2021 Anshul Patel
+This code is licensed under MIT license (see LICENSE.MD for details)
+
+@author: cheapBuy
+"""
+
+from bs4 import BeautifulSoup
+import requests
+
+class FetchDescription():
+    """
+    A class used to fetch description by parsing the URL
+    
+    ...
+
+    Attributes
+    ----------
+    product_link : str
+        link of the product
+        
+    Methods
+    -------
+    fetch_desc_walmart:
+        Fetch description from Walmart
+    fetch_desc_amazon:
+        Fetch description from Amazon
+    fetch_desc_ebay:
+        Fetch description from Ebay
+    fetch_desc_bjs:
+        Fetch description from bjs
+    fetch_desc_costco:
+        Fetch description from Costco
+    """
+    
+    def __init__(self,product_link):
+        """
+        Parameters
+        ----------
+        product_link : str
+            link of the product
+        """
+        self.product_link = product_link
+    
+    def fetch_desc_walmart(self):
+        """ 
+        Fetch description from Walmart
+        """
+        description = ''
+        try:
+            link = self.product_link.replace("https://www.walmart.com/ip/","")
+            for ch in link:
+                if(ch!="/"):
+                    description += ch
+                else:
+                    break
+            description  = description.replace('-',' ')
+        except:
+            description = ''
+        return description
+    
+    def fetch_desc_amazon(self):
+        """ 
+        Fetch description from Amazon
+        """
+        description = ''
+        try:
+            link = self.product_link.replace('https://www.amazon.com/','')
+            for ch in link:
+                if ch!='/':
+                    description+=ch
+                else:
+                    break
+            description = description.replace('-', ' ')
+        except:
+            description = ''
+        return description
+    
+    def fetch_desc_ebay(self):
+        """
+        Fetch description from Ebay
+        """
+        try:
+            html =  requests.get(self.product_link)
+            soup=BeautifulSoup(html.content,'html.parser')
+            product = soup.find("div",{"class" : "vi-swc-lsp"}, { "id" : "itemTitle" })
+            title = product.find('span', class_="u-dspn").text.strip()
+        except:
+            title = ''
+        return title 
+    
+    def fetch_desc_costco(self):
+        """ 
+        Fetch description from Costco
+        """
+        description = ''
+        try:
+            link = self.product_link.replace("https://www.costco.com/","")
+            for ch in link:
+                if(ch!="."):
+                    description += ch
+                else:
+                    break
+            description  = description.replace('-',' ')
+        except:
+            description = ''
+        return description
+    
+    def fetch_desc_bjs(self):
+        """ 
+        Fetch description from BJs
+        """
+        description = ''
+        try:
+            link = self.product_link.replace("https://www.bjs.com/product/","")
+            for ch in link:
+                if(ch!="/"):
+                    description += ch
+                else:
+                    break
+            description  = description.replace('-',' ')
+        except:
+            description=''
+        return description
+    
---
+++ source/Final/web_scrappers/WebScrapper.py
@@ -0,0 +1,86 @@
+"""
+Copyright (c) 2021 Anshul Patel
+This code is licensed under MIT license (see LICENSE.MD for details)
+
+@author: cheapBuy
+"""
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from source.web_scrappers.FetchDescription import FetchDescription
+from source.web_scrappers.WebScrapper_Amazon import WebScrapper_Amazon
+from source.web_scrappers.WebScrapper_Bjs import WebScrapper_Bjs
+from source.web_scrappers.WebScrapper_Ebay import WebScrapper_Ebay
+from source.web_scrappers.WebScrapper_Costco import WebScrapper_Costco
+from source.web_scrappers.WebScrapper_Walmart import WebScrapper_Walmart
+
+class WebScrapper:
+    
+    def __init__(self,product_link):
+        self.product_link = product_link
+    
+    def get_driver(self):
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
+        return driver
+
+    def get_description(self):
+        if 'walmart' in self.product_link:
+            source = 'walmart'
+            fd = FetchDescription(self.product_link)
+            description = fd.fetch_desc_walmart()
+        elif 'amazon' in self.product_link:
+            source = 'amazon'
+            fd = FetchDescription(self.product_link)
+            description = fd.fetch_desc_amazon()
+        elif 'ebay' in self.product_link:
+            source = 'ebay'
+            fd = FetchDescription(self.product_link)
+            description = fd.fetch_desc_ebay()
+        elif 'costco' in self.product_link:
+            source = 'costco'
+            fd = FetchDescription(self.product_link)
+            description = fd.fetch_desc_costco()
+        elif 'bjs' in self.product_link:
+            source= 'bjs'
+            fd = FetchDescription(self.product_link)
+            description = fd.fetch_desc_bjs()
+        else:
+            source = 'N/A'
+        if source != 'N/A':
+            return description
+    
+    def call_scrapper(self):
+        product_description = self.get_description()
+        print(product_description)
+        
+        t_amazon = WebScrapper_Amazon(product_description)
+        t_walmart = WebScrapper_Walmart(product_description)
+        t_ebay = WebScrapper_Ebay(product_description)
+        t_bjs = WebScrapper_Bjs(product_description)
+        t_costco = WebScrapper_Costco(product_description)
+        
+        t_amazon.start()
+        t_walmart.start()
+        t_ebay.start()
+        t_bjs.start()
+        t_costco.start()
+        
+        t_amazon.join()
+        t_walmart.join()
+        t_ebay.join()
+        t_bjs.join()
+        t_costco.join()
+        
+        results_amazon = t_amazon.result
+        results_walmart = t_walmart.result
+        results_ebay = t_ebay.result
+        results_bjs = t_bjs.result
+        results_costco = t_costco.result
+        
+        return [results_amazon, results_walmart, results_ebay, results_bjs, results_costco]
+
+#link = 'https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038'
+#link = 'https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038'
+#ws = WebScrapper(link)
+#result=ws.call_scrapper()        
